d2404159eb4a9a2b636dc08f8dea333be6099268
RogerioBorba <idehco3@gmail.com>
2015-09-18 00:02:43 -0300

    teste3

--- mappingTool/static/mappingTool/mapping.js
+++ mappingTool/static/mappingTool/mapping.js
@@ -1,39 +1,109 @@
 /**
  * Created by Rogério on 13/09/2015.
  */
-var map = L.map('map').setView([-22, -41], 2);
-var geoJsonObjects = [];
+/** initialization map and variables **/
+var map = L.map('map', initializeContextMenuMap()).setView([-21.2858, -41.78682], 2);
+var geoJsonLayers = [];
+var actuallayer;
 function initMapping() {
     mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-    osmAdress =  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    opts = {attribution: 'Map data &copy; ' + mapLink, maxZoom: 18 };
-    L.tileLayer( osmAdress, opts ).addTo(map);
-
+    osmAdress = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    opts = {attribution: 'Map data &copy; ' + mapLink, maxZoom: 18};
+    L.tileLayer(osmAdress, opts).addTo(map);
 }
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
+
+
+var geojsonFeature =[
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Coors Field",
+            "amenity": "Baseball Stadium",
+            "popupContent": "This is where the Rockies play!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.99404, 39.75621]
+        }
     },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
+    {
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+    },
+    {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
     }
-};
+];
+function initializeContextMenuMap() {
+    return {
+        zoom: 15,
+        contextmenu: true,
+        contextmenuWidth: 140,
+        contextmenuItems: [{
+            text: 'Show coordinates',
+            callback: showCoordinates
+        }, {
+            text: 'Center map here',
+            callback: centerMap
+        }, '-', {
+            text: 'Zoom in',
+            callback: zoomIn
+        }, {
+            text: 'Zoom out',
+            callback: zoomOut
+        }]
+    }
+}
+
+function showCoordinates (e) {
+    console.log(e.latlng);
+}
+function centerMap (e) {
+    map.panTo(e.latlng);
+}
+
+function zoomIn (e) {
+    map.zoomIn();
+}
+
+function zoomOut (e) {
+    map.zoomOut();
+}
+
+/** end initialization map and variables **/
 
 function loadGeoJson() {
     var aGeoJsonLayer = L.geoJson(geojsonFeature, {
        onEachFeature: onEachFeature
     }).addTo(map);
-    geoJsonObjects.push(aGeoJsonLayer);
-    console.log(geoJsonObjects);
+    geoJsonLayers.push(aGeoJsonLayer);
+
 }
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
+    layer.on('click', function() { clickOnLayer(feature, layer)});
+
+}
+
+function clickOnLayer(feature, layer ){
+    console.log(feature.type == 'Feature');
+    actuallayer = layer;
+    binderMenuContextTo(layer);
+
+}
+
+function binderMenuContextTo(layer) {
+    layer.bindContextMenu({
+        contextmenu: true,
+        contextmenuInheritItems: true,
+        contextmenuItems: [
+             {
+                text: 'Edit attributes',
+                callback: function () { editingAttributes(layer);  }
+            }, {
+                separator: true
+            }]
+    });
+
 }
